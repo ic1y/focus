ded1cc0d66c50111a94d78346c005de23244a1af
Icey <73022629+IceBearBots@users.noreply.github.com>
2024-06-05 22:19:34 +0800

    add install button

--- static/install.js
+++ static/install.js
@@ -1,3 +1,21 @@
 if ("serviceWorker" in navigator) {
 	navigator.serviceWorker.register("/sw.js");
 }
+let installPrompt = null;
+const installButton = document.getElementById("install");
+
+installButton.addEventListener("click", async () => {
+	if (installPrompt === null) {
+		return;
+	}
+	const result = await installPrompt.prompt();
+	console.log(`Install prompt was: ${result.outcome}`);
+	installPrompt = null;
+	installButton.style.display = "none";
+});
+
+window.addEventListener("beforeinstallprompt", (event) => {
+	event.preventDefault();
+	installPrompt = event;
+	installButton.style.display = "block";
+});
